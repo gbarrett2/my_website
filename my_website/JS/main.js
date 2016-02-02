a7595a84917b39dbd0c0e18f18dc8c62fa740c1f
gbarrett2 <gab.barrett@gmail.com>
2016-02-02 14:23:35 -0600

    working on it

--- my_website/JS/main.js
+++ my_website/JS/main.js
@@ -1,6 +1,11 @@
-//Example 2.3 line 6...function to create a table with cities and their populations
+//initialize function called when the script loads
+function initialize(){
+    cities();
+};
+
+//function to create a table with cities and their populations
 function cities(){
-    //define an array of objects for cities and population
+    //define two arrays for cities and population
     var cityPop = [
         { 
             city: 'Madison',
@@ -20,22 +25,23 @@ function cities(){
         }
     ];
 
-    //...
+    //append the table element to the div
+    $("#mydiv").append("<table>");
 
-    //Example 2.3 line 41...loop to add a new row for each city
+    //append a header row to the table
+    $("table").append("<tr>");
+
+    //add the "City" and "Population" columns to the header row
+    $("tr").append("<th>City</th><th>Population</th>");
+
+    //loop to add a new row for each city
     for (var i = 0; i < cityPop.length; i++){
-        var tr = document.createElement("tr");
-
-        var city = document.createElement("td");
-        city.innerHTML = cityPop[i].city; //NOTE DIFFERENT SYNTAX
-        tr.appendChild(city);
-
-        var pop = document.createElement("td");
-        pop.innerHTML = cityPop[i].population; //NOTE DIFFERENT SYNTAX
-        tr.appendChild(pop);
-
-        table.appendChild(tr);
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        $("table").append(rowHtml);
     };
 };
 
-cities();
+//call the initialize function when the document has loaded
+$(document).ready(initialize);
