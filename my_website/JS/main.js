eb39ee894641f30c57a5d998cd715434cb77bfb3
gbarrett2 <gab.barrett@gmail.com>
2016-02-02 22:00:23 -0600

    changesdebug

--- my_website/JS/main.js
+++ my_website/JS/main.js
@@ -41,7 +41,65 @@ function cities(){
         //add the row's html string to the table
         $("table").append(rowHtml);
     };
+
+
 };
 
 //call the initialize function when the document has loaded
 $(document).ready(initialize);
+
+function addColumns(cityPop){
+    
+    $('tr').each(function(i){
+
+        if (i == 0){
+
+            $(this).apend('<th>City Size</th>');
+        } else {
+
+            var citySize;
+
+            if (cityPop[i-1].population < 100000){
+                citySize = 'Small';
+
+            } else if (cityPop[i-1].population < 500000){
+                citysize = 'Medium';
+
+            } else {
+                citySize = 'Large';
+            };
+
+            $this.append('<td' + citySize + '</td>');
+        };
+    });
+};
+
+function addEvents(){
+
+    $('#table').mouseover(function(){
+        
+        var color = "rgb(";
+
+        for (var i=0; i<3; i++){
+
+            var random = Math.round(Math.random() * 255);
+
+            color += "random";
+
+            if (i<2){
+                color += ",";
+            
+            } else {
+                color += ")";
+        };
+
+        $(this).css('color', color);
+    });
+
+    function clickme(){
+
+        alert('Hey, you clicked me!');
+    };
+
+    $('table').on('click', clickme);
+};
