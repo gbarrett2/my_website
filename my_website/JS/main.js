5b0411976657216fdfa75de1f2216c74fce178c1
gbarrett2 <gab.barrett@gmail.com>
2016-02-10 11:39:48 -0600

    Module 3 Activity

--- my_website/JS/main.js
+++ my_website/JS/main.js
@@ -1,6 +1,7 @@
 //initialize function called when the script loads
 function initialize(){
-    cities();
+    //cities();
+    debugAjax();
 };
 
 //function to create a table with cities and their populations
@@ -41,99 +42,106 @@ function cities(){
         //add the row's html string to the table
         $('table').append(rowHtml);
     };
-    addColumns(cityPop);
-    addEvents();
+  //  addColumns(cityPop);
+  //  addEvents();
     //calls functions
-    debugAjax();
-    debugCallback();
+
 };
 
 //call the initialize function when the document has loaded
-$("#mydiv").ready(initialize);
+//$("#mydiv").ready(initialize);
 
 
-//function to add a column saying the size of the population
-function addColumns(cityPop){
-//for each table row call function
-    $('tr').each(function(i){
-//organize column for each type of population
-        if (i == 0){
-//append title, City Size, to table
-            $(this).append('<th>City Size</th>');
-            i++;
-        }
-        else {
+// //function to add a column saying the size of the population
+// function addColumns(cityPop){
+// //for each table row call function
+//     $('tr').each(function(i){
+// //organize column for each type of population
+//         if (i == 0){
+// //append title, City Size, to table
+//             $(this).append('<th>City Size</th>');
+//             i++;
+//         }
+//         else {
+//
+//             var citySize;
+//             if (cityPop[i-1].population < 2400000){
+//                 citySize = 'Small';
+//
+//
+//             } else if (cityPop[i-1].population < 3700000){
+//                 citySize = 'Medium';
+//
+//
+//             } else {
+//                 citySize = 'Large';
+//
+//             };
+// //append citySize to table
+//             $(this).append('<td>' + citySize + '</td>');
+//         };
+//     });
+// };
+// //function to place a random color when you mouseover the table
+// function addEvents(){
+//
+// //create mouseover function
+//     $('table').mouseover(function(){
+// //define color variable
+//         var color = "rgb(";
+// //selects random color
+//         for (var i = 0; i < 3; i++){
+//
+//             var random = Math.round(Math.random() * 255);
+//
+//             color += random;
+//
+//             if (i < 2){
+//                 color += ',';
+//
+//             } else {
+//                 color += ')';
+//         };
+//
+//         $(this).css('color', color);
+//     };
+// });
+// //function that allows you to click elements of the table
+// function clickme(){
+// //initalizes the click me event
+//         alert('Hey, you clicked me!');
+//     };
+//
+//     $('table').on('click', clickme);
+//     $('table').on('click', function(){
+//     });
+// };
 
-            var citySize;
-            if (cityPop[i-1].population < 2400000){
-                citySize = 'Small';
 
-
-            } else if (cityPop[i-1].population < 3700000){
-                citySize = 'Medium';
-
-
-            } else {
-                citySize = 'Large';
-
-            };
-//append citySize to table
-            $(this).append('<td>' + citySize + '</td>');
-        };
-    });
-};
-//function to place a random color when you mouseover the table
-function addEvents(){
-
-//create mouseover function
-    $('table').mouseover(function(){
-//define color variable
-        var color = "rgb(";
-//selects random color
-        for (var i = 0; i < 3; i++){
-
-            var random = Math.round(Math.random() * 255);
-
-            color += random;
-
-            if (i < 2){
-                color += ',';
-
-            } else {
-                color += ')';
-        };
-
-        $(this).css('color', color);
-    };
-});
-//function that allows you to click elements of the table
-function clickme(){
-//initalizes the click me event
-        alert('Hey, you clicked me!');
-    };
-
-    $('table').on('click', clickme);
-    $('table').on('click', function(){
-    });
-};
-
-//function that calls the ajax method
-function debugCallback(response){
-//views response
-	$(mydiv).append('GeoJSON data: ' + JSON.stringify(response));
-};
 //defines the Ajax function
 function debugAjax(){
 
 //basic jQuery ajax method
 	$.ajax("data/MegaCity.geojson", {
 		dataType: "json",
-		success: function(response, status, jpXHRobject){
+		success: function(response){
       debugCallback(response);
-      //views response
+      //views response on the website
       $(mydiv).append('<br>GeoJSON data:<br>' + JSON.stringify(response));
 		}
 	});
 };
-//views repsonse
-$(mydiv).append('GeoJSON data: ' + JSON.stringify(response));
+//function that calls the ajax method
+ function debugCallback(response){
+//response is being accessed here (replaced mydata)
+  console.log(response);
+ };
+
+//this calls initializer, basically starts the whole code
+$(document).ready(initialize);
+
+//by making the response a variable when you call it outside of the brackets
+// it becomes undefined, instead of not defined
+var response;
+//response can't be accessed outside of the function
+console.log(response);
